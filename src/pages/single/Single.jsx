93a0a3a74de965acfb33a42ad5dbe908544298dd
PedroSetti <sr.setti@outlook.com>
2023-02-14 21:19:59 -0300

    create a single component

--- src/pages/single/Single.jsx
+++ src/pages/single/Single.jsx
@@ -1,7 +1,19 @@
 import "./single.scss"
+import {Sidebar} from "../../components/sidebar/Sidebar"
+import {Navbar} from "../../components/navbar/Navbar"
 
 export const Single = () => {
     return (
-        <div>Single</div>
+        <div className="single">
+          <Sidebar/>
+          <div className="singleContainer">
+            <Navbar/>
+            <div className="top">
+              <div className="left"></div>
+              <div className="right"></div>
+            </div>
+            <div className="bottom"></div>
+          </div>
+        </div>
     )
 }
